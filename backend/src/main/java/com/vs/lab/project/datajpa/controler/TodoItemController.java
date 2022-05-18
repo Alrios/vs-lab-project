a12193a8faea21210320db8960ba5e7707630198
Alrios <felipe.sibila@gmail.com>
2022-05-18 17:29:32 +0200

    Updated Controler - PUT /todos/ (send/receive JSON object., e.g. can change priority)

--- backend/src/main/java/com/vs/lab/project/datajpa/controler/TodoItemController.java
+++ backend/src/main/java/com/vs/lab/project/datajpa/controler/TodoItemController.java
@@ -6,20 +6,20 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 
 @RestController
-@RequestMapping("/todo")
+@RequestMapping("/todos")
 public class TodoItemController {
 
     @Autowired
@@ -44,8 +44,37 @@ public class TodoItemController {
   }
 
     @GetMapping("/{name}")
-    public Optional<TodoItem> getTodoItemById(@PathVariable String name) {
+    public ResponseEntity<Optional<TodoItem>> getTodoItemById(@PathVariable String name) {
         Optional<TodoItem> todoItem = todoItemRepository.findById(name);
-        return todoItem;
+        if (todoItem.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(todoItem, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{name}")
+    public ResponseEntity<Optional<TodoItem>> deleteTodoItem(@PathVariable String name) {
+
+        Optional<TodoItem> todoItem = todoItemRepository.findById(name);
+        if (todoItem.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        todoItemRepository.deleteById(name);
+
+        return new ResponseEntity<>(todoItem, HttpStatus.OK);
+    }
+
+    @PutMapping()
+    public ResponseEntity<TodoItem> updateEmployee(@RequestBody TodoItem todoItemToUpdate) {
+        String id = todoItemToUpdate.getTodo();
+        if (!todoItemRepository.existsById(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        
+        TodoItem todoItem = todoItemRepository.getById(id);
+        todoItem.setPriority(todoItemToUpdate.getPriority());
+        final TodoItem updatedTodoItem = todoItemRepository.save(todoItem);
+        return new ResponseEntity<>(updatedTodoItem, HttpStatus.OK);
     }
 }
